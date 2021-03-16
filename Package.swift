7728030bb598f4c422d609df4a60b252306e1ebf
Pablo Viciano <pablo.viciano@gmail.com>
2021-03-16 21:36:00 +0100

    other

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "AFramework",
+    name: "TestAFramework",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AFramework",
-            targets: ["AFramework"]),
+            name: "TestAFramework",
+            targets: ["TestAFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,6 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "AFramework", path: "TestAFramework.xcframework")
+        .binaryTarget(name: "TestAFramework", path: "TestAFramework.xcframework")
     ]
 )
