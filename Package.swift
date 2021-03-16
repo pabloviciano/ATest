a43d35db69e9dcd85efdc1ce9d2f43d23d15d845
Pablo Viciano <pablo.viciano@gmail.com>
2021-03-16 20:14:24 +0100

    other

--- Package.swift
+++ Package.swift
@@ -18,6 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "AFramework", url: "https://github.com/pabloviciano/ATest/blob/main/A.xcframework.zip", checksum: "0ed9f9d058485c636590203f73450afb6429ed8c8aee52364010ad16ff5b5e84")
+        .binaryTarget(name: "AFramework", url: "https://github.com/pabloviciano/ATest/blob/master/A.xcframework.zip", checksum: "c47a8d0ac5cc8986a3c8d9e0b0499884a1c29ccae4f3fbe4d0e77d72b50244b1")
     ]
 )
