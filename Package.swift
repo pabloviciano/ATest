625dfc480fd47f61f3feff9d77734e3267203ff9
Pablo Viciano <pablo.viciano@gmail.com>
2021-03-16 21:34:07 +0100

    other

--- Package.swift
+++ Package.swift
@@ -18,6 +18,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "AFramework", path: "A.xcframework")
+        .binaryTarget(name: "AFramework", path: "TestAFramework.xcframework")
     ]
 )
