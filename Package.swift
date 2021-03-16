371e58fdd8a498dcf497c9f7e7e0163d81314fab
Pablo Viciano <pablo.viciano@gmail.com>
2021-03-16 20:16:29 +0100

    other

--- Package.swift
+++ Package.swift
@@ -7,9 +7,6 @@ let package = Package(
     name: "AFramework",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "AFramework",
-            targets: ["AFramework"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,6 +15,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "AFramework", path: "A.xcframework.zip")
+        .binaryTarget(name: "A.xcframework", path: "A.xcframework.zip")
     ]
 )
