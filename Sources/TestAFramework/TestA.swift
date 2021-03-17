502d68082e9ff9284271dd5c9f78fe1636209464
Pablo Viciano <pablo.viciano@gmail.com>
2021-03-17 07:59:02 +0100

    commit

---
+++ Sources/TestAFramework/TestA.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pablo Viciano Negre on 16/03/2021.
+//
+
+import Foundation
+
+public struct TestA {
+    public init() { }
+    public func print() -> String { "Soy A" }
+}
